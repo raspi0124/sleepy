6a1bf180b3d24b599540abcbae6998d8233b1a76
raspi0124 <adores0124@yahoo.co.jp>
2017-01-14 01:24:10 +0100

    Fixed Bug

--- sidebar.php
+++ sidebar.php
@@ -1,26 +1,6 @@
 <?php
-/**
- * The sidebar containing the main widget area
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
 
 
-if (is_front_page()) {
-    echo "	<div id='home_sidebar' class='widget-area' role='complementary'>
-    		<?php dynamic_sidebar( 'home_sidebar' ); ?>
-	</div>";
-}else {
-    echo "";
-}
-?>
-
-<?php
 if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
